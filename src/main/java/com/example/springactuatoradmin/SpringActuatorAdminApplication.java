5b5a9c82356839f2aeba2f5f4d43d750ae1354b1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-11-14 00:40:22 +0700

    Enable Admin

--- src/main/java/com/example/springactuatoradmin/SpringActuatorAdminApplication.java
+++ src/main/java/com/example/springactuatoradmin/SpringActuatorAdminApplication.java
@@ -3,7 +3,9 @@ package com.example.springactuatoradmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication @EnableAdminServer
 public class SpringActuatorAdminApplication {
 
 	public static void main(String[] args) {
