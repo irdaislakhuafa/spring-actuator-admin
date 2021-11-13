04927479113f355574be598c83e2959247796300
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-11-13 23:58:44 +0700

    First commit

---
+++ src/main/java/com/example/springactuatoradmin/SpringActuatorAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.springactuatoradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springactuatoradmin/SpringActuatorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springactuatoradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
